2a5d673917c3e7e418716b51b1ffcbdcaa02c856
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2017-01-22 20:20:53 +0000

    Compiles, links and runs with BGFX library and initialisation.

--- noisebgfx.cpp
+++ noisebgfx.cpp
@@ -1,8 +1,46 @@
 #include <Windows.h>
 #include "SDL.h"
+#include "SDL_syswm.h"
+
+#include "bx/bx.h"
+#include "bgfx/platform.h" // it must be included after SDL to enable SDL
+                           // integration code path.
+#include "bgfx/bgfx.h"
 
 #pragma comment (lib, "sdl2.lib")
 #pragma comment (lib, "sdl2main.lib")
+
+
+inline bool sdlSetWindow(SDL_Window* _window)
+{
+	SDL_SysWMinfo wmi;
+	SDL_VERSION(&wmi.version);
+	if (!SDL_GetWindowWMInfo(_window, &wmi) )
+	{
+		return false;
+	}
+
+	bgfx::PlatformData pd;
+#	if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
+	pd.ndt          = wmi.info.x11.display;
+	pd.nwh          = (void*)(uintptr_t)wmi.info.x11.window;
+#	elif BX_PLATFORM_OSX
+	pd.ndt          = NULL;
+	pd.nwh          = wmi.info.cocoa.window;
+#	elif BX_PLATFORM_WINDOWS
+	pd.ndt          = NULL;
+	pd.nwh          = wmi.info.win.window;
+#	elif BX_PLATFORM_STEAMLINK
+	pd.ndt          = wmi.info.vivante.display;
+	pd.nwh          = wmi.info.vivante.window;
+#	endif // BX_PLATFORM_
+	pd.context      = NULL;
+	pd.backBuffer   = NULL;
+	pd.backBufferDS = NULL;
+	bgfx::setPlatformData(pd);
+
+	return true;
+}
 
 
 /*
@@ -23,6 +61,9 @@ int main(int argc, char *argv[])
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
     SDL_Window *sdl_window = SDL_CreateWindow("SDL2 + BGFX", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
+	sdlSetWindow(sdl_window);
+
+	bgfx::init();
 
 	 // Main loop
     bool done = false;
@@ -47,6 +88,7 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
+#ifdef WIN32
 int CALLBACK WinMain(
     HINSTANCE   hInstance,
     HINSTANCE   hPrevInstance,
@@ -56,3 +98,4 @@ int CALLBACK WinMain(
 {
 	return main(0, nullptr);
 }
+#endif // WIN32
