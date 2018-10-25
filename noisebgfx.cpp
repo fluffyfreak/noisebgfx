ab60afa5caf51b487eb079f63398dbf738da80a3
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2018-10-25 22:56:29 +0100

    So much hacking to get back to being not quite functional

--- noisebgfx.cpp
+++ noisebgfx.cpp
@@ -1,5 +1,8 @@
 #ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
   #include <Windows.h>
+#undef min
+#undef max
   #include "SDL.h"
   #include "SDL_syswm.h"
 #else
@@ -7,7 +10,7 @@
   #include "SDL2/SDL_syswm.h"
 #endif
 #include "bx/bx.h"
-#include "bx/fpumath.h"
+#include "bx/math.h"
 #include "bx/timer.h"
 #include "bgfx/platform.h" // it must be included after SDL to enable SDL
 // integration code path.
@@ -81,6 +84,52 @@ static const uint16_t s_cubeTriStrip[] =
 	5,
 };
 
+static const uint16_t s_cubeLineList[] =
+{
+	0, 1,
+	0, 2,
+	0, 4,
+	1, 3,
+	1, 5,
+	2, 3,
+	2, 6,
+	3, 7,
+	4, 5,
+	4, 6,
+	5, 7,
+	6, 7,
+};
+
+static const uint16_t s_cubeLineStrip[] =
+{
+	0, 2, 3, 1, 5, 7, 6, 4,
+	0, 2, 6, 4, 5, 7, 3, 1,
+	0,
+};
+
+static const uint16_t s_cubePoints[] =
+{
+	0, 1, 2, 3, 4, 5, 6, 7
+};
+
+static const char* s_ptNames[]
+{
+	"Triangle List",
+	"Triangle Strip",
+	"Lines",
+	"Line Strip",
+	"Points",
+};
+
+static const uint64_t s_ptState[]
+{
+	UINT64_C(0),
+	BGFX_STATE_PT_TRISTRIP,
+	BGFX_STATE_PT_LINES,
+	BGFX_STATE_PT_LINESTRIP,
+	BGFX_STATE_PT_POINTS,
+};
+
 
 inline bool sdlSetWindow(SDL_Window* _window)
 {
@@ -112,6 +161,39 @@ inline bool sdlSetWindow(SDL_Window* _window)
 
 	return true;
 }
+
+/*static bgfx::ShaderHandle loadShader(bx::FileReaderI* _reader, const char* _name)
+{
+	char filePath[512];
+
+	const char* shaderPath = "???";
+
+	switch (bgfx::getRendererType())
+	{
+	case bgfx::RendererType::Noop:
+	case bgfx::RendererType::Direct3D9:  shaderPath = "shaders/dx9/";   break;
+	case bgfx::RendererType::Direct3D11:
+	case bgfx::RendererType::Direct3D12: shaderPath = "shaders/dx11/";  break;
+	case bgfx::RendererType::Gnm:        shaderPath = "shaders/pssl/";  break;
+	case bgfx::RendererType::Metal:      shaderPath = "shaders/metal/"; break;
+	case bgfx::RendererType::OpenGL:     shaderPath = "shaders/glsl/";  break;
+	case bgfx::RendererType::OpenGLES:   shaderPath = "shaders/essl/";  break;
+	case bgfx::RendererType::Vulkan:     shaderPath = "shaders/spirv/"; break;
+
+	case bgfx::RendererType::Count:
+		BX_CHECK(false, "You should not be here!");
+		break;
+	}
+
+	bx::strCopy(filePath, BX_COUNTOF(filePath), shaderPath);
+	bx::strCat(filePath, BX_COUNTOF(filePath), _name);
+	bx::strCat(filePath, BX_COUNTOF(filePath), ".bin");
+
+	bgfx::ShaderHandle handle = bgfx::createShader(loadMem(_reader, filePath));
+	bgfx::setName(handle, filePath);
+
+	return handle;
+}*/
 
 
 /*
@@ -156,20 +238,62 @@ int main(int argc, char *argv[])
 		, 0
 	);
 
+	bgfx::VertexBufferHandle m_vbh;
+	bgfx::IndexBufferHandle m_ibh[BX_COUNTOF(s_ptState)];
+	bgfx::ProgramHandle m_program;
+	int32_t m_pt = 0;
+
+	// Create program from shaders.
+	{
+		//bgfx::createShader(
+		bgfx::ShaderHandle vsh = BGFX_INVALID_HANDLE;// loadShader(_reader, _vsName);
+		bgfx::ShaderHandle fsh = BGFX_INVALID_HANDLE;
+		//if (NULL != _fsName)
+		//{
+		//	fsh = loadShader(_reader, _fsName);
+		//}
+		m_program = bgfx::createProgram(vsh, fsh, true /* destroy shaders when program is destroyed */);
+		//m_program = loadProgram("vs_cubes", "fs_cubes");
+	}
+
 	// Create vertex stream declaration.
 	PosColorVertex::init();
 
 	// Create static vertex buffer.
-	bgfx::VertexBufferHandle m_vbh = bgfx::createVertexBuffer(
+	m_vbh = bgfx::createVertexBuffer(
 		// Static data can be passed with bgfx::makeRef
 		bgfx::makeRef(s_cubeVertices, sizeof(s_cubeVertices))
 		, PosColorVertex::ms_decl
 	);
 
-	// Create static index buffer.
-	bgfx::IndexBufferHandle m_ibh = bgfx::createIndexBuffer(
+	// Create static index buffer for triangle list rendering.
+	m_ibh[0] = bgfx::createIndexBuffer(
+		// Static data can be passed with bgfx::makeRef
+		bgfx::makeRef(s_cubeTriList, sizeof(s_cubeTriList))
+	);
+
+	// Create static index buffer for triangle strip rendering.
+	m_ibh[1] = bgfx::createIndexBuffer(
 		// Static data can be passed with bgfx::makeRef
 		bgfx::makeRef(s_cubeTriStrip, sizeof(s_cubeTriStrip))
+	);
+
+	// Create static index buffer for line list rendering.
+	m_ibh[2] = bgfx::createIndexBuffer(
+		// Static data can be passed with bgfx::makeRef
+		bgfx::makeRef(s_cubeLineList, sizeof(s_cubeLineList))
+	);
+
+	// Create static index buffer for line strip rendering.
+	m_ibh[3] = bgfx::createIndexBuffer(
+		// Static data can be passed with bgfx::makeRef
+		bgfx::makeRef(s_cubeLineStrip, sizeof(s_cubeLineStrip))
+	);
+
+	// Create static index buffer for point list rendering.
+	m_ibh[4] = bgfx::createIndexBuffer(
+		// Static data can be passed with bgfx::makeRef
+		bgfx::makeRef(s_cubePoints, sizeof(s_cubePoints))
 	);
 
 	// Create program from shaders.
@@ -207,26 +331,12 @@ int main(int argc, char *argv[])
 		float eye[3] = { 0.0f, 0.0f, -35.0f };
 
 		// Set view and projection matrix for view 0.
-		const bgfx::HMD* hmd = bgfx::getHMD();
-		if ( NULL != hmd && 0 != (hmd->flags & BGFX_HMD_RENDERING) )
-		{
-			float view[16];
-			bx::mtxQuatTranslationHMD(view, hmd->eye[0].rotation, eye);
-			bgfx::setViewTransform(0, view, hmd->eye[0].projection, BGFX_VIEW_STEREO, hmd->eye[1].projection);
-
-			// Set view 0 default viewport.
-			//
-			// Use HMD's width/height since HMD's internal frame buffer size
-			// might be much larger than window size.
-			bgfx::setViewRect(0, 0, 0, hmd->width, hmd->height);
-		}
-		else
 		{
 			float view[16];
 			bx::mtxLookAt(view, eye, at);
 
 			float proj[16];
-			bx::mtxProj(proj, 60.0f, float(m_width) / float(m_height), 0.1f, 100.0f);
+			bx::mtxProj(proj, 60.0f, float(m_width) / float(m_height), 0.1f, 100.0f, bgfx::getCaps()->homogeneousDepth);
 			bgfx::setViewTransform(0, view, proj);
 
 			// Set view 0 default viewport.
@@ -237,13 +347,26 @@ int main(int argc, char *argv[])
 		// if no other draw calls are submitted to view 0.
 		bgfx::touch(0);
 
+		bgfx::IndexBufferHandle ibh = m_ibh[m_pt];
+		uint64_t state = 0
+			| BGFX_STATE_WRITE_R
+			| BGFX_STATE_WRITE_G
+			| BGFX_STATE_WRITE_B
+			| BGFX_STATE_WRITE_A
+			| BGFX_STATE_WRITE_Z
+			| BGFX_STATE_DEPTH_TEST_LESS
+			| BGFX_STATE_CULL_CW
+			| BGFX_STATE_MSAA
+			| s_ptState[m_pt]
+			;
+
 		// Submit 11x11 cubes.
-		/*for ( uint32_t yy = 0; yy < 11; ++yy )
+		for (uint32_t yy = 0; yy < 11; ++yy)
 		{
-			for ( uint32_t xx = 0; xx < 11; ++xx )
+			for (uint32_t xx = 0; xx < 11; ++xx)
 			{
 				float mtx[16];
-				bx::mtxRotateXY(mtx, time + xx*0.21f, time + yy*0.37f);
+				bx::mtxRotateXY(mtx, time + xx * 0.21f, time + yy * 0.37f);
 				mtx[12] = -15.0f + float(xx)*3.0f;
 				mtx[13] = -15.0f + float(yy)*3.0f;
 				mtx[14] = 0.0f;
@@ -252,19 +375,16 @@ int main(int argc, char *argv[])
 				bgfx::setTransform(mtx);
 
 				// Set vertex and index buffer.
-				bgfx::setVertexBuffer(m_vbh);
-				bgfx::setIndexBuffer(m_ibh);
+				bgfx::setVertexBuffer(0, m_vbh);
+				bgfx::setIndexBuffer(ibh);
 
 				// Set render states.
-				bgfx::setState(0
-					| BGFX_STATE_DEFAULT
-					| BGFX_STATE_PT_TRISTRIP
-				);
+				bgfx::setState(state);
 
 				// Submit primitive for rendering to view 0.
 				bgfx::submit(0, m_program);
 			}
-		}*/
+		}
 
 		// Advance to next frame. Rendering thread will be kicked to
 		// process submitted rendering primitives.
@@ -273,9 +393,14 @@ int main(int argc, char *argv[])
 
 
 	// Cleanup
-	bgfx::destroyIndexBuffer(m_ibh);
-	bgfx::destroyVertexBuffer(m_vbh);
-	//bgfx::destroyProgram(m_program);
+	// Cleanup.
+	for (uint32_t ii = 0; ii < BX_COUNTOF(m_ibh); ++ii)
+	{
+		bgfx::destroy(m_ibh[ii]);
+	}
+
+	bgfx::destroy(m_vbh);
+	bgfx::destroy(m_program);
 
 	// Shutdown bgfx.
 	bgfx::shutdown();
